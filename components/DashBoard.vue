887f1ad59fdbd044a233bb5098452d416204f02e
arvinoids-laptop <arvinoids@gmail.com>
2023-01-11 05:22:56 -0500

    initial commot

---
+++ components/DashBoard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div v-for="group in groups" :key="group" class="flex flex-col rounded-2xl text-center p-4 m-4 w-[200px] shadow-lg bg-gray-100">
+            <ProductCard :groupId="group.id" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+const app = useNuxtApp().$pb
+
+const groups = await app.collection('groups').getList(1)
+
+</script>
