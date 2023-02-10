c3b1af580017a3b235c003eff3cf25b3a1a5391d
arvinoids-laptop <arvinoids@gmail.com>
2023-02-10 14:57:17 -0500

    aa

--- components/AddCase.vue
+++ components/AddCase.vue
@@ -1,43 +1,56 @@
 <template>
-<div class="modal" :class="{'modal-open':modalIsOpen}" :id="props.groupId">
-  <div class="modal-box w-2/3 max-w-5xl">
-    <h3 class="font-bold text-lg">Assign ticket to {{ props.groupDescription }}</h3>
-    <p class="py-4">The ticket will be assigned to {{ props.nextUserName }}</p>
-      <input type="text" placeholder="CAS-XXXXXXXXXXX" class="input input-bordered input-sm w-auto" v-model="caseId">
-      <div class="modal-action justify-center">
-       <a :href="'#'+skipAnchorCatch" class="btn btn-outline">Skip - Catch up later</a>
-       <a :href="'#'+skipAnchorOut" class="btn btn-outline" >Skip - Out of office</a>
-       <button class="btn" :disabled="caseId ===''" @click="submitCase()">Assign</button>
-       <a href="#" class="btn btn-warning">Cancel</a>
+  <div class="modal" :id="props.groupId">
+    <div class="modal-box w-2/3 max-w-5xl">
+      <h3 class="font-bold text-lg">Assign ticket to {{ props.groupDescription }}</h3>
+      <div class="flex flex-col items-center">
+        <p class="py-4">The ticket will be assigned to {{ props.nextUserName }}</p>
+        <input type="text" placeholder="CAS-XXXXXXXXXXX" class="input input-bordered my-2 w-[300px]" v-model="caseId">
+        <div v-if="message" class="alert alert-success my-2 w-max"
+          :class="{ 'alert-error': submitStatus === 'failed' }">{{
+            message
+          }}</div>
       </div>
+      <div class="modal-action justify-center">
+        <a :href="'#' + skipAnchorCatch" class="btn btn-outline">Skip - Catch up later</a>
+        <a :href="'#' + skipAnchorOut" class="btn btn-outline">Skip - Out of office</a>
+        <label class="btn btn-success" @click="submitCase">Assign</label>
+        <a href="#" class="btn btn-warning">Cancel</a>
+      </div>
+    </div>
   </div>
-</div>
-<SkipCatch :userId="props.nextUserId"  :id="skipAnchorCatch" :groupId ="props.groupId" :fullName="props.nextUserName"/>
-<SkipOut :userId="props.nextUserId"  :id="skipAnchorOut" :groupId ="props.groupId" :fullName="props.nextUserName"/>
+  <SkipCatch :userId="props.nextUserId" :id="skipAnchorCatch" :groupId="props.groupId" :fullName="props.nextUserName" />
+  <SkipOut :userId="props.nextUserId" :id="skipAnchorOut" :groupId="props.groupId" :fullName="props.nextUserName" />
+  <CaseValidation :user="props.nextUserId" :id="props.groupId + props.nextUserId" :group="props.groupId"
+    :fullName="props.nextUserName" />
 
 </template>
     
 <script setup lang='ts'>
 const props = defineProps<{
-    groupId: string,
-    groupDescription: string,
-    nextUserId: string,
-    nextUserName: string
+  groupId: string,
+  groupDescription: string,
+  nextUserId: string,
+  nextUserName: string
 }>()
-const router = useRouter()
-const caseId = ref('')
+const caseId = useCaseId()
 let str1 = props.groupId
 let str2 = props.nextUserId
-const skipAnchorCatch:string = str1!.concat(str2!,"Catch")
-const skipAnchorOut:string = str1!.concat(str2!,"Out")
-const reloadKey = useRefresher()
-const modalIsOpen=ref(false)
+const skipAnchorCatch: string = str1!.concat(str2!, "Catch")
+const skipAnchorOut: string = str1!.concat(str2!, "Out")
+const message = ref('')
+const submitStatus = ref('')
 
 async function submitCase() {
-  await assignCase(caseId.value,props.nextUserId,props.groupId)
-  reloadKey.value.count++
-  modalIsOpen.value=false
-  console.log("is modal open?",modalIsOpen.value)
+  const res = await assignCase(caseId.value, props.nextUserId, props.groupId)
+  message.value = res.message
+  submitStatus.value = res.status
+  if (submitStatus.value === 'success') {
+    setTimeout(() => {
+      window.location.reload();
+      console.log("reloading window");
+    }, 1000);
+  }
 }
+
 
 </script>
--- composables/functions.ts
+++ composables/functions.ts
@@ -3,6 +3,10 @@ import PocketBase from 'pocketbase'
 const cnf = useRuntimeConfig().public
 const pb = new PocketBase(cnf.pocketBaseURL)
 pb.autoCancellation(false)
+interface result {
+    status: string,
+    message: string
+}
 
 // When the dashboard loads, the system looks for users under each group from the currentlist collection. 
 //If there are no users, the system creates the currentlist by running a query from the counter sorted by count.
@@ -17,33 +21,33 @@ function createFilter(field: string, value: string) {
     return filter
 }
 
-async function createCounter(user: string, group: string) {
-    const count = getCount(user, group)
-    const data = {
-        "user": user,
-        "group": group,
-        "count": count
-    }
-    try {
-        await pb.collection('counter').create(data);
-    } catch (error) { console.log(error) }
-}
+// async function createCounter(user: string, group: string) {
+//     const count = getCount(user, group)
+//     const data = {
+//         "user": user,
+//         "group": group,
+//         "count": count
+//     }
+//     try {
+//         await pb.collection('counter').create(data);
+//     } catch (error) { console.log(error) }
+// }
 
 async function updateCounter(group: string, user: string) {
     let groupFilter = createFilter('group', group)
     let userFilter = createFilter('user', user)
     let newCount = await getCount(user, group)
-    console.log('new count: ',newCount)
+    console.log('new count: ', newCount)
     let data = {
         "user": user,
         "group": group,
         "count": newCount
     }
     const record = await pb.collection('counter').getFirstListItem(groupFilter + "&&" + userFilter)
-    console.log('counter record ',record)
+    console.log('counter record ', record)
     try {
         let res = await pb.collection('counter').update(record.id, data)
-        console.log('result',res)
+        console.log('result', res)
     } catch (e) { console.log(e) }
 }
 
@@ -54,39 +58,39 @@ async function getCount(user: string, group: string) {
     return res.totalItems
 }
 
-async function getGroupUsers(group: string) {
-    const groupFilter = "group='" + group + "'"
-    const res = await pb.collection('cases').getList(1, 10000, { "filter": groupFilter, '$autoCancel': false })
-    if (res.totalItems > 0) {
-        const users = res.items.map((ele: { user: any; }) => ele.user).filter((ele: any, i: any, arr: string | any[]) => arr.indexOf(ele) == i); //pick unique users https://stackoverflow.com/questions/53720210/how-to-get-unique-value-of-keys-from-array-of-object-using-javascript
-        //console.log("users for group "+group+": ",users)   
-        return users
-    }
+// async function getGroupUsers(group: string) {
+//     const groupFilter = "group='" + group + "'"
+//     const res = await pb.collection('cases').getList(1, 10000, { "filter": groupFilter, '$autoCancel': false })
+//     if (res.totalItems > 0) {
+//         const users = res.items.map((ele: { user: any; }) => ele.user).filter((ele: any, i: any, arr: string | any[]) => arr.indexOf(ele) == i); //pick unique users https://stackoverflow.com/questions/53720210/how-to-get-unique-value-of-keys-from-array-of-object-using-javascript
+//         //console.log("users for group "+group+": ",users)   
+//         return users
+//     }
 
-}
+// }
 
-async function counterExists(user: string, group: string) {
-    const groupFilter = "group='" + group + "'"
-    const userFilter = "user='" + user + "'"
-    const counter = await pb.collection('counter').getList(1, 10000, { "filter": groupFilter + "&&" + userFilter, '$autoCancel': false })
-    if (counter.totalItems === 0) return false
-    else return true
-}
+// async function counterExists(user: string, group: string) {
+//     const groupFilter = "group='" + group + "'"
+//     const userFilter = "user='" + user + "'"
+//     const counter = await pb.collection('counter').getList(1, 10000, { "filter": groupFilter + "&&" + userFilter, '$autoCancel': false })
+//     if (counter.totalItems === 0) return false
+//     else return true
+// }
 
-export const useCreateGroupList = async (group: string) => {
-    console.log("running creategroupcounter")
-    const users = await getGroupUsers(group)
-    console.log(`users for group ${group}:`, users)
-    if (users!.length === 0) return
-    else {
-        users!.forEach((user: string) => {
-            if (!counterExists(user, group))
-                try { createCounter(user, group); console.log("counter created") }
-                catch (e) { console.log(e) }
-        })
-    }
+// export const useCreateGroupList = async (group: string) => {
+//     console.log("running creategroupcounter")
+//     const users = await getGroupUsers(group)
+//     console.log(`users for group ${group}:`, users)
+//     if (users!.length === 0) return
+//     else {
+//         users!.forEach((user: string) => {
+//             if (!counterExists(user, group))
+//                 try { createCounter(user, group); console.log("counter created") }
+//                 catch (e) { console.log(e) }
+//         })
+//     }
 
-}
+// }
 
 // for realtime assign
 export async function assignCase(caseId: string, user: string, group: string) {
@@ -96,18 +100,25 @@ export async function assignCase(caseId: string, user: string, group: string) {
         "case": caseId,
         "assignedBy": pb.authStore.model!.username
     }
+
+    const result = { message: '', status: '' }
+    if (caseId === '') return { message: 'Case ID cannot be blank', status: 'failed' }
     if (await caseExists(caseId)) {
-        return "This case has already been assigned. Please check."
+        result.message = "This case has already been assigned. Please double-check and try again."
+        result.status = "failed"
+        return result
     } else {
         await pb.collection('cases').create(data)
         await updateCounter(group, user)
         await createCurrentList(group)
-        return "Case has been saved. The owner should receive a notification shortly."
+        result.message = "Case has been saved. The owner should receive a notification shortly."
+        result.status = "success"
+        return result
     }
 }
 
 //case checking if it exists
-async function caseExists(caseId: string) {
+export async function caseExists(caseId: string) {
     const caseFilter = 'case="' + caseId + '"'
     try {
         const record = await pb.collection('cases').getFirstListItem(caseFilter)
@@ -116,15 +127,6 @@ async function caseExists(caseId: string) {
         return false
     }
 }
-
-// returns the case cont for specific user in a group
-// async function reRunCount(userId: string, groupId: string) {
-//     const groupFilter = 'group="' + groupId + '"'
-//     const userFilter = 'user="' + userId + '"'
-//     const combinedFilter = groupFilter + " && " + userFilter
-//     const cases = await pb.collection('cases').getList(1, 500, { filter: combinedFilter })
-//     return cases.totalItems
-// }
 
 async function deleteCurrentList(groupId: string) {
     console.log('deleting currentlist for ', groupId)
@@ -152,42 +154,26 @@ export async function createCurrentList(groupId: string) {
 
 
 
-// To skip and catch up
-// export async function useSkipCatch(groupId:string) {
-//     console.log('executing skip catch up later')
-//     const groupFilter:string = "group="+'"'+groupId+'"'
-//     let record = await pb.collection('currentposition').getFirstListItem(groupFilter,{ '$autoCancel': false })
-//     let id:string = record.id
-//     let position:number = record.position
-//     try {
-//       await pb.collection('currentposition').update(id,{ "group": groupId, "position": position+1},{ '$autoCancel': false })
-//       if(await isPositionExceeded(groupId)){
-//         await resetPosition(id)
-//       }
-//     } catch (e) { console.log(e) }
+// async function isPositionExceeded(groupId: string) {
+//     console.log('checking if position has exceeded')
+//     const groupFilter: string = "group=" + '"' + groupId + '"'
+//     const currentlist = await pb.collection('currentlist').getList(1, 1000, { filter: groupFilter })
+//     let max = currentlist.totalItems
+//     let rec = await pb.collection('currentposition').getFirstListItem(groupFilter, { '$autoCancel': false })
+//     let position = rec.position
+//     if (position > max) {
+//         console.log(position > max)
+//         createCurrentList(groupId!)
+//         return true
+//     } else return false
 // }
 
-
-async function isPositionExceeded(groupId: string) {
-    console.log('checking if position has exceeded')
-    const groupFilter: string = "group=" + '"' + groupId + '"'
-    const currentlist = await pb.collection('currentlist').getList(1, 1000, { filter: groupFilter })
-    let max = currentlist.totalItems
-    let rec = await pb.collection('currentposition').getFirstListItem(groupFilter, { '$autoCancel': false })
-    let position = rec.position
-    if (position > max) {
-        console.log(position > max)
-        createCurrentList(groupId!)
-        return true
-    } else return false
-}
-
-async function resetPosition(recordId: string) {
-    console.log("resetting position of ", recordId)
-    try {
-        await pb.collection('currentposition').update(recordId, { "position": 1 }, { '$autoCancel': false })
-    } catch (e) { console.log(e) }
-}
+// async function resetPosition(recordId: string) {
+//     console.log("resetting position of ", recordId)
+//     try {
+//         await pb.collection('currentposition').update(recordId, { "position": 1 }, { '$autoCancel': false })
+//     } catch (e) { console.log(e) }
+// }
 
 export async function useSkipOut(userId: string, groupId: string) {
     const random = Math.random().toString(36).substring(3, 9)
@@ -203,14 +189,6 @@ export async function useSkipOut(userId: string, groupId: string) {
     await createCurrentList(groupId)
     location.reload()
 }
-
-// async function useSkipCatch(groupId: string) {
-//     console.log('executing skip catch up later')
-//     const groupFilter: string = "group=" + '"' + groupId + '"'
-//     let users = await pb.collection('currentlist').getList(1, 1000, { filter: groupFilter, expand: "user", '$autoCancel': false })
-
-// }
-
 export async function getUserCases(userId?: string) {
     console.log('getting cases for user ', userId)
     let cases;
@@ -219,4 +197,64 @@ export async function getUserCases(userId?: string) {
         cases = await pb.collection('cases').getList(1, 1000, { filter: userFilter, expand: "user, group", sort: "-created" })
     }
     return cases
+}
+
+export async function useUpdateCase(recordId: string, user: string, group: string, caseId: string, assignedBy: string) {
+    const data = {
+        "user": user,
+        "group": group,
+        "case": caseId,
+        "assignedBy": assignedBy
+    }
+    try {
+        const res = await pb.collection('cases').update(recordId, data)
+        return "Case updated."
+    } catch (e: any) { return e.message }
+}
+
+export async function useDeleteCase(id: string) {
+    const result: result = { message: '', status: 'success' }
+    try {
+        await pb.collection('cases').delete(id)
+        result.message = `Case ${id} is deleted`
+        useRefreshAll()
+        return result
+    } catch (e: any) {
+        result.status = "failed";
+        result.message = e.message
+        return result
+    }
+}
+
+export async function useSubmitCase(caseId: string, user: string, group: string) {
+    const res = await assignCase(caseId, user, group)
+    const result = { message: res.message, submitStatus: res.status }
+    if (res.status === 'success') {
+        setTimeout(() => {
+            window.location.reload();
+            console.log("reloading window");
+        }, 1000);
+    }
+    return result
+}
+
+export async function useRefreshAll() {
+    //get groups
+    const rec = await pb.collection('groups').getList(1, 100)
+    const groups = rec.items
+    //foreach group, get users
+    groups.forEach(
+        async (group) => {
+            const userRec = await pb.collection('users').getList(1, 100, { filter: `memberOf~"${group.id}"` })
+            const users = userRec.items
+            //for each user, update counter
+            users.forEach(
+                async (user) => {
+                    await updateCounter(group.id, user.id)
+                }
+            )
+            //createcurrentlist for group
+            await createCurrentList(group.id)
+        }
+    )
 }
