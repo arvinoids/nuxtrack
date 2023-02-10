e2845187654c152793c9c5fbcdd92a380d31dd01
arvinoids-laptop <arvinoids@gmail.com>
2023-02-10 14:57:11 -0500

    working proto

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,15 +20,26 @@ module.exports = {
         "accent": "#1C64B4",
         "neutral": "#CACACA",
         "base-100": "#EBEBEB",
-        "base-200": "#8A8A8A",
-        "base-300": "#32323C",
+        "base-200": "#CACACA",
+        "base-300": "#8a8a8a",
         "info": "#006446",
         "success": "#3AF23A",
         "warning": "#FAA519",
         "error": "#dc2626",
         "--btn-text-case":"none"
-      }
+      },
+      darkLex: {
+        "primary": "#a3e635", 
+        "secondary": "#15803d",
+        "accent": "#a7f3d0",
+        "neutral": "#1B1D1D",    
+        "base-100": "#212121",
+        "info": "#2563EB",
+        "success": "#16A34A",
+        "warning": "#D97706",
+        "error": "#DC2626",
+        },
     }, "emerald", "forest"],
-    darkTheme: "forest"
+    darkTheme: "darkLex"
   }
 }
