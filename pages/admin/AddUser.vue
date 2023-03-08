0ef03336ec0c3723415519377d68f67df3029727
arvinoids-mac <arvinoids@gmail.com>
2023-03-08 12:07:54 -0500

    deleted unused and added adduser

--- pages/admin/AddUser.vue
+++ pages/admin/AddUser.vue
@@ -1,4 +1,23 @@
-<template></template>
+<template>
+  <div class="flex flex-col items-center gap-2">
+    <h1 class="text-lg text-center">Add User</h1>
+    <div class="border m-2v p-2">
+      <form action="submit flex flex-col">
+        <label for="name">Name: </label>
+        <input type="text" class="input" placeholder="username" />
+        <label for="username">Username: </label>
+        <input type="text" class="input" placeholder="username" />
+        <label for="email">Email: </label>
+        <input type="text" class="input" placeholder="email" />
+        <label for="password">Password: </label>
+        <input type="password" class="input" placeholder="password" />
+        <label for="confirm">Confirm password: </label>
+        <input type="password" class="input" placeholder="confirm" />
+      </form>
+    </div>
+    <p>This is the adduser page...</p>
+  </div>
+</template>
 
 <script setup lang="ts"></script>
 
