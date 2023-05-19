1852cbd1894a3d0095fb9d4d1c45024908540912
arvinoids-laptop <arvinoids@gmail.com>
2023-05-19 14:19:38 -0400

    updated admin module naming

--- pages/admin/Cases.vue
+++ pages/admin/Cases.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center gap-5">
     <AdminCaseStats />
-    <CreateDummyCases />
+    <AdminCreateDummyCases />
   </div>
 </template>
 
--- pages/admin/Users.vue
+++ pages/admin/Users.vue
@@ -1,5 +1,5 @@
 <template>
-  <UsersAdmin />
+  <AdminUsers />
 </template>
 
 <script setup lang="ts"></script>
