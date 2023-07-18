273765ddc6266bb68a69b85c042f6863e503fd03
arvinoids-laptop <arvinoids@gmail.com>
2023-07-18 09:10:05 -0400

    copyright cosmetic change

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
     <p class="text-center text-sm pt-5">
       &copy; 2023
       <a href="https://solutionsteam.lrdc.lexmark.com/"
-        >Rotation Tracker Ver. {{ value }}Lexmark Solutions Support</a
+        >&copy; Lexmark Solutions Support</a
       >
     </p>
     <p class="text-xs text-center text-base-200 italic">Rotation Tracker v{{ value }}</p>
