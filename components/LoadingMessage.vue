f42afdbdb1de9a2337189c45fa432f7d9fc1becc
arvinoids-laptop <arvinoids@gmail.com>
2023-11-08 03:49:57 +0800

    added loading spinner for page transitions

---
+++ components/LoadingMessage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="absolute w-screen z-50 h-screen flex items-center justify-center">
+    <div
+      class="flex items-center justify-center relative p-10 bg-base-100 border gap-4 z-50 mx-10 backdrop-blur-2xl shadow-xl w-[500px]"
+    >
+      <Icon name="svg-spinners:180-ring-with-bg" size="3rem" class="text-secondary" />
+      <p>Loading data. Please wait...</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
