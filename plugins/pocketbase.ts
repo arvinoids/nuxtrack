c1a69bb5b5ae0f5d2ca5e31f8b6f27a7f0549fce
arvinoids-laptop <arvinoids@gmail.com>
2024-01-30 23:24:46 +0800

    updated pocketBase localstorage name and env file

--- plugins/pocketbase.ts
+++ plugins/pocketbase.ts
@@ -1,8 +1,8 @@
-import PocketBase from 'pocketbase'
+import PocketBase, { LocalAuthStore } from 'pocketbase'
 
 export default defineNuxtPlugin(nuxtApp => {
     const cnf = useRuntimeConfig().public
-    const client = new PocketBase(cnf.pocketBaseURL)
+    const client = new PocketBase(cnf.pocketBaseURL, new LocalAuthStore(cnf.pbStorage))
     client.autoCancellation(false)
     return {
         provide: {
