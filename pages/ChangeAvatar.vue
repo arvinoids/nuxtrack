587c3d9c5e90c5657b4b1928025faa075537bcbf
amagaway on work laptop <amagaway@lexmark.com>
2025-06-10 01:22:32 +0800

    cosmetic update on ChangeAvatar

--- pages/ChangeAvatar.vue
+++ pages/ChangeAvatar.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="flex flex-col items-center gap-2">
+  <div class="flex flex-col items-center gap-2 mt-2">
     <h1 class="text-lg text-center font-bold">Change Avatar</h1>
 
-    <div class="flex flex-row border m-2 p-12 shadow-md w-full gap-10 flex-wrap">
+    <div
+      class="flex flex-row border border-neutral-200 m-2 p-12 shadow-md w-full gap-10 flex-wrap"
+    >
       <div class="flex flex-col items-stretch gap-10">
         <div class="flex flex-col items-center">
           <div class="font-semibold">Current Avatar</div>
@@ -11,9 +13,11 @@
           </div>
           <div v-else>None</div>
         </div>
-        <div class="flex flex-col items-center border p-8">
+        <div class="flex flex-col items-center bg-neutral p-8">
           <div class="font-semibold mb-2">Upload new avatar</div>
-          <div class="badge p-3 m-2">Please keep your avatar to 300x300px or smaller</div>
+          <div class="alert alert-warning p-3 m-2">
+            Please keep your avatar to 300x300px or smaller
+          </div>
           <div class="form-control gap-3 flex flex-col items-center">
             <input
               type="file"
