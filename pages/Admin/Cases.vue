f975d551c7bc111719ae40272347fd3670ccd87b
amagaway on work laptop <amagaway@lexmark.com>
2026-01-12 22:08:58 +0800

    updated caniuse and added devonly to case archiver

--- pages/Admin/Cases.vue
+++ pages/Admin/Cases.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center gap-5">
     <AdminCreateDummyCases v-if="currentUser && currentUser.role === 'lead'" />
-    <AdminCaseArchiver />
+    <DevOnly><AdminCaseArchiver /></DevOnly>
     <AdminCaseStats />
     <AdminCasesReporter />
   </div>
